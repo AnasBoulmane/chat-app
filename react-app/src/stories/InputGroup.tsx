edb79f5f84614299941aeffc774c19b5406b4254
aboulmane <aboulmane@sqli.com>
2021-01-04 22:28:46 +0100

    chore(story): Refactor InputGroup - use svg component

--- react-app/src/stories/InputGroup.tsx
+++ react-app/src/stories/InputGroup.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CheckmarkIcon } from "svgs/CheckmarkIcon";
 import { FormGroup, Input } from "./InputGroup.style";
 
 export interface InputGroupProps {
@@ -52,11 +53,7 @@ export const InputGroup = ({
                 title="Cliquez pour sauvegarder, Échap pour annuler"
                 style={{ transform: "scaleX(1) scaleY(1)", opacity: 1 }}
               >
-                <span data-testid="checkmark" data-icon="checkmark" className="">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <path fill="currentColor" d="M9 17.2l-4-4-1.4 1.3L9 19.9 20.4 8.5 19 7.1 9 17.2z" />
-                  </svg>
-                </span>
+                <CheckmarkIcon data-testid="checkmark" data-icon="checkmark" />
               </div>
             </span>
           )}
